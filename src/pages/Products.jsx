0eee082f949d91e57bb771611cbff38b47c4275e
shakibchakladar <shakibchakladar@gmail.com>
2024-11-16 17:07:41 +0600

    Implement our product

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,15 @@
+import FeaturedCard from "../compoments/FeaturedCard";
 
 const Products = () => {
     return (
         <div>
-            <h2 className="text-4xl my-4">Products </h2>
+            <h2 className="my-4 text-4xl">Our Products </h2>
+            <div className="grid gap-3 md:grid-cols-3">
+                <FeaturedCard/>
+                <FeaturedCard/>
+                <FeaturedCard/>
+            </div>
+            
         </div>
     );
 };

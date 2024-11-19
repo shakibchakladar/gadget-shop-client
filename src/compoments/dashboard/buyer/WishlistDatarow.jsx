312eb0905eb6894a60abdeae21ebdd3f3e2eb6c7
shakibchakladar <shakibchakladar@gmail.com>
2024-11-19 20:04:49 +0600

    implement my wishlist

--- src/compoments/dashboard/buyer/WishlistDatarow.jsx
+++ src/compoments/dashboard/buyer/WishlistDatarow.jsx
@@ -2,28 +2,28 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import useUserData from "../../../hooks/useUserData";
 
-const WishlistDatarow = ({ wishItem,setLatestData }) => {
-    const userData=useUserData();
-    const userEmail=userData?.email;
-    const handleRemoveFromWishlist = async() => {
-        await axios
-           .patch("http://localhost:4000/wishlist/remove", {
-             userEmail: userEmail,
-             productId: wishItem?._id,
-           })
-           .then((res) => {
-             if(res.data.modifiedCount){
-               Swal.fire({
-                 position: "top-end",
-                 icon: "success",
-                 title: "Product delete from wishlist",
-                 showConfirmButton: false,
-                 timer: 1500
-               });
-               setLatestData((prev)=>!prev)
-             }
-           });
-       };
+const WishlistDatarow = ({ wishItem, setLatestData }) => {
+  const userData = useUserData();
+  const userEmail = userData?.email;
+  const handleRemoveFromWishlist = async () => {
+    await axios
+      .patch("http://localhost:4000/wishlist/remove", {
+        userEmail: userEmail,
+        productId: wishItem?._id,
+      })
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Product delete from wishlist",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          setLatestData((prev) => !prev);
+        }
+      });
+  };
   return (
     <tr>
       <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
@@ -45,17 +45,14 @@ const WishlistDatarow = ({ wishItem,setLatestData }) => {
         </div>
       </td>
       <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">
-          {wishItem?.category}
-        </p>
+        <p className="text-gray-900 whitespace-no-wrap">{wishItem?.category}</p>
       </td>
       <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">
-          ${wishItem?.price}
-        </p>
+        <p className="text-gray-900 whitespace-no-wrap">${wishItem?.price}</p>
       </td>
       <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <button onClick={handleRemoveFromWishlist}
+        <button
+          onClick={handleRemoveFromWishlist}
           //   onClick={() => setIsDeleteOpen(true)}
           aria-label="Delete property"
           className="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900 cursor-pointer"
@@ -66,13 +63,6 @@ const WishlistDatarow = ({ wishItem,setLatestData }) => {
           ></span>
           <span className="relative">Remove</span>
         </button>
-        {/* Delete modal */}
-        {/* <DeleteModal
-          isOpen={isDeleteOpen}
-          closeModal={closeDeleteModal}
-          handleDelete={handleDelete}
-          id={property?._id} // Ensure using `_id` for consistency
-        /> */}
       </td>
       <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
         <button
